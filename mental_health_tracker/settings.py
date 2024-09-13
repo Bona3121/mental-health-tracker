779e2aba608b0c986d61ddc3b813057e4f5727e6
Bona3121 <marlamarlena999@gmail.com>
2024-09-13 16:57:28 +0700

    update link pws

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-5p4%)r64t877wh=3m###4eec0-n5t^#vv$v8zrym^fm5vp=-2#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "http://marla-marlena-mentalhealthtracker2.pbp.cs.ui.ac.id", "https://marla-marlena-mentalhealthtracker2.pbp.cs.ui.ac.id, marla-marlena-mentalhealthtracker2.pbp.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "http://marla-marlena-mentalhealthtracker2.pbp.cs.ui.ac.id", "https://marla-marlena-mentalhealthtracker2.pbp.cs.ui.ac.id", "marla-marlena-mentalhealthtracker2.pbp.cs.ui.ac.id"]
 
 
 # Application definition
